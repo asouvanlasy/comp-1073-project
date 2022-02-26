d4c8b84e304722cc9afab885763c028c53776205
asouvanlasy <asouvanlasy@outlook.com>
2022-02-26 12:42:33 -0500

    Can render 4 different charts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import LineChart from "./components/LineChart.js";
+import BarChart from "./components/BarChart";
+import PieChart from "./components/PieChart.js";
+import ScatterChart from "./components/ScatterChart";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+      <div>
+        <LineChart />
+        <BarChart />
+        <PieChart />
+        <ScatterChart />
+      </div>
+    );   
 }
-
-export default App;
